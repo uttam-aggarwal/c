e256f85e2fcff0142eabc76a71dbcf51fbf510d3
uttam-aggarwal <115714793+uttam-aggarwal@users.noreply.github.com>
2022-10-18 16:05:59 +0530

    added loop.c

---
+++ loop.c
@@ -0,0 +1,15 @@
+//for loop
+#include <stdio.h>
+int main(){
+    //for(initalisation;condition;updation)
+    for(int i=1;i<=5;i=i+1){
+        printf("hello world \n");
+ }
+ for(int i=1;i<=199;i=i+1){
+    printf("%d \n",i);
+ }
+ for(int i=199;i>=1;i=i-1){
+    printf("%d \n",i);
+ }
+    
+}
