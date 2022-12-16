d2cdf91df76f80842cc5f91d0f505b4381a08053
uttam-aggarwal <115714793+uttam-aggarwal@users.noreply.github.com>
2022-12-16 20:45:24 +0530

    --

--- try.c
+++ try.c
@@ -1,6 +1,9 @@
  #include <stdio.h>
  int main(){
-    int a = 1.89700000;
-    printf("%d",a);
-    return 0;
- }
+   //  int a = 1.89700000;
+   //  printf("%d",a);
+   //  return 0;
+   int age=33;
+   int *ptr=&age;//pointer=address of s vsriable
+   int _age=*ptr; //the thing stored at that address , here 33
+   }
