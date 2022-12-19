a0fdb348b8a6b4bf2e7a02e602cc145fa898bec0
uttam-aggarwal <115714793+uttam-aggarwal@users.noreply.github.com>
2022-12-19 13:35:35 +0530

    --

--- try.c
+++ try.c
@@ -12,7 +12,7 @@
    printf("%p\n",&age);//%p use to print address
    printf("%u\n",&age);//%u use to convert that into a more readable integer
    printf("%u\n",ptr);//
-   printf("%u\n",&ptr);
+   printf("%u\n",&ptr);//
    printf("%u\n",&_age);
 
    
