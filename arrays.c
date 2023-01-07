34fdee2744f0b68b3febda9cb5a516893d73d5f3
uttam-aggarwal <115714793+uttam-aggarwal@users.noreply.github.com>
2023-01-07 20:24:39 +0530

    --

---
+++ arrays.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    //initializing methods-
+    //int main[]={96,97,98};//the size is specified by counting the values
+    //printf("the values present in the array is %d,%d,%d\n",main[0],main[1],main[2]);
+    //int main[3]={40,56,78};//specificing the size of array beforehand
+    //printf("the values present in the array is %d,%d,%d\n",main[0],main[1],main[2]);
+    //storing values in a array-//
+    int main[3];
+    scanf("%d",&main[0]);
+    scanf("%d",&main[1]);
+    scanf("%d",&main[2]);
+    printf("the values present in the array is %d,%d,%d\n",main[0],main[1],main[2]);
+}
