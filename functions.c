79a938b6c9f63fde99836769fbcafc2e15097244
uttam-aggarwal <115714793+uttam-aggarwal@users.noreply.github.com>
2023-02-04 11:02:32 +0530

    --

--- functions.c
+++ functions.c
@@ -2,10 +2,13 @@
 //declaration/prototype
 void printhello();
 void printgoodbye();
+int sum(int a,int b);
 int main(){
     //calling a function
     printhello();
     printgoodbye();
+    printf("%d",sum(8,9));
+    return 0;
 }
 //defining a function
 void printhello(){
@@ -13,4 +16,7 @@ void printhello(){
 }
 void printgoodbye(){
     printf("good Bye!\n");
+}
+int sum(int a,int b){
+    return (a+b);
 }
--- pyramid.c
+++ pyramid.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
+// int main(){
+//     for (int i=0;i<8;i++){
+//         for(int j=0;j<i;j++){
+//             printf("*");
+//         }
+//     printf("\n");
+//     }
+// }
 int main(){
-    for (int i=0;i<8;i++){
-        for(int j=0;j<i;j++){
+    int n=10;
+    int t=1;
+    for(int i=1;i<7;i++){
+        for(int k=0;k<(n/2);k++){
+            printf(" ");
+        }
+        for(int j=0;j<t;j++){
             printf("*");
         }
-    printf("\n");
+        for(int l=0;l<(n/2);l++){
+            printf(" ");
+        }
+        n=n-2;
+        t=t+2;
+        printf("\n");
     }
 }
