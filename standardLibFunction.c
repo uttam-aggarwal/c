6aaba57c27bad65734fb035c2ac6800611c05d05
uttam-aggarwal <115714793+uttam-aggarwal@users.noreply.github.com>
2023-02-08 23:52:24 +0530

    --

--- standardLibFunction.c
+++ standardLibFunction.c
@@ -2,5 +2,10 @@
 #include <stdio.h>
 #include <string.h>
 int main(){
-    
+    char name[]="uttam";
+    int length=strlen(name);//came from string.h
+    printf("%d",length);
+    //strcopy(newstr,oldstr)copy value if old to new
+    //strcat(a.b) add a+b but size of a (in []braackes) should be more than len a + len b
+    //strcmp(a,b) compare too strs return 0 for equal , some +ve number if a>b and -ve for a<b
 }
