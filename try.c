bdd2c6cdb91cc00496774db1a70481bfc25fc4a2
uttam-aggarwal <115714793+uttam-aggarwal@users.noreply.github.com>
2023-02-23 14:08:04 +0530

    --

--- try.c
+++ try.c
@@ -12,26 +12,32 @@ int main()
     5 4 4 4 4 4 4 4 5
     5 5 5 5 5 5 5 5 5
     */
-   int n,a=-1;
-   scanf("%d",&n);
-   for(int i =0;i<n;i++){
-    a=a+2;
-   }
-   printf("%d\n",a);
-   
-   for(int i=1;i<=a;i++){
-    for(int j=1;j<=a;j++){
-        for(int k=0;k<n;k++){
-            if(i==n-k||i==n+k){
-                printf("%d ",n+k-1);
-                break;
-            }
-            else if(j==n-k||j==n+k){
-                printf("%d ",n+k-1);
-                break;
-            }}
-        }
+    int n, a = -1;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        a = a + 2;
     }
-    printf("\n");//
-   }
+    printf("%d\n", a);
+
+    for (int i = 1; i <= a; i++)
+    {
+        for (int j = 1; j <= a; j++)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                if(i!=n){
+                if (i == n - k || i == n + k)
+                {
+                    printf("%d ", n+k);
+                }}
+                else
+                {
+                    if(j == n - k || j == n + k){/
+                    printf("%d ", n + k - 1);}
+                }
+            }
+        }
+        printf("\n");
+    }
 }
