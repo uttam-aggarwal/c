c413da1ffdd3c838e57dbaa4643a80b71ca9197c
uttam-aggarwal <115714793+uttam-aggarwal@users.noreply.github.com>
2023-03-02 09:18:16 +0530

    --

--- printingtokens.c
+++ printingtokens.c
@@ -1,17 +1,35 @@
+// #include <stdio.h>
+// #include <string.h>
+// #include <math.h>
+// #include <stdlib.h>
+
+// int main() {
+
+//     // char s[100];
+//     // fgets(s,100,stdin);
+//     // for(int i=0;s[i]!='\0';i++){
+//     //     s[i+1]==' '?printf("%c",s[i]):printf("%c",s[i]);
+//     // }
+//     // return 0;
+//     // printf("hi");
+//     // printf("\n");
+//     // printf("hi");
+//     int sum=2+4/2+6*2;
+//     printf("%d",sum);
+// }
 #include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
-
+void solve() 
+{
+    int ch = 2;
+    switch(ch) 
+  {
+        case 1: printf("1 ");
+        case 2: printf("2 ");
+        case 3: printf("3 ");
+        default: printf(" None");
+    }
+} // end of solve() function
 int main() {
-
-    // char s[100];
-    // fgets(s,100,stdin);
-    // for(int i=0;s[i]!='\0';i++){
-    //     s[i+1]==' '?printf("%c",s[i]):printf("%c",s[i]);
-    // }
-    // return 0;
-    printf("hi");
-    printf("\n");
-    printf("hi");
+    solve();
+	return 0;
 }
