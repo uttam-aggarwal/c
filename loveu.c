fb26eed1f8c9f21d0156407196eb731b3a40c2af
uttam-aggarwal <115714793+uttam-aggarwal@users.noreply.github.com>
2023-03-07 09:02:30 +0530

    --

---
+++ loveu.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    // int a=8;
+    // if(a>10)
+    //     if(a%2==0)
+    //     printf("Even");
+    // else printf("odd");
+    // else printf("wrong");
+    int sum;
+    sum=2<<3+5&&2+9%2*6;//
+    printf("%d",sum);
+    
+    return 0;
+}
