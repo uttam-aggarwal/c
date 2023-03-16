cf4f1f51e0bea9275b96f311c76dafba20f653a8
uttam-aggarwal <115714793+uttam-aggarwal@users.noreply.github.com>
2023-03-16 19:30:41 +0530

    --

--- arthematic.c
+++ arthematic.c
@@ -3,6 +3,6 @@ int main(){
     int a,b;
     scanf("%d",&a);
     scanf("%d",&b);
-    printf("add-(%d)\nsub-(%d)\nmultiplication-(%d)\n",a+b,a-b,a*b);//
+    printf("add-(%d)\nsub-(%d)\nmultiplication-(%d)\n",a+b,a-b,a*b);
     b==0?printf("divison is not possible"):printf("division-(%d)\nmodulous-(%d)",a/b,a%b);
 }
