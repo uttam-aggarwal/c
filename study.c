d899ff13a4dfaa649bf893dc572b9762da6435e2
uttam-aggarwal <115714793+uttam-aggarwal@users.noreply.github.com>
2023-03-18 11:47:05 +0530

    --

--- study.c
+++ study.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+int func1(int a,int b,int c );
 int main(){
-    double a=3&&5&4%3;  
-    // float a= 654.1239;
-    // printf("%.3f",a);
-    printf("%lf",a);//
+    // double a=3&&5&4%3;  
+    // // float a= 654.1239;
+    // // printf("%.3f",a);
+    // printf("%lf",a);//
 
+    func1(1,2,3);
+    }
+int func1(int a,int b,int c ){
+    a>b?printf("%d",c):return c;
 
 }
