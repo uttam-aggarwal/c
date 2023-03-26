2c3246e8720a8ff9c93742d251e67a2c6b79a0fd
uttam-aggarwal <115714793+uttam-aggarwal@users.noreply.github.com>
2023-03-26 09:32:24 +0530

    --

--- CWPATTERn.c
+++ CWPATTERn.c
@@ -9,7 +9,7 @@ void pattern1(){
     int n,a;
     scanf("%d",&n);
     for(int i=1;i<=n;i++){
-        a=1;
+        a=1;//
         for(int j=0;j<i;j++){
             printf("%d  ",a);
             a++;
