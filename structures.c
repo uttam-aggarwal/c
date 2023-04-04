e91359193ed7dd98f928da975dfcd9fb8cced2b8
uttam-aggarwal <115714793+uttam-aggarwal@users.noreply.github.com>
2023-04-04 14:03:44 +0530

    --

--- structures.c
+++ structures.c
@@ -2,8 +2,8 @@
 #include <string.h>
 struct student{
     int roll;
-    float marks;
-    char name[100];
+    float marks,none;
+    char name[100];//
 };//don't forget this semi-colon here
 // -----------------------------------------
 //you can create alias of a struct name (short form for it like the following code)
@@ -15,7 +15,7 @@ struct student{
 int main(){
     // struct student ECE[100];
     // ECE[0].roll=1994;
-    // ECE[0].marks=98.9;//
+    // ECE[0].marks=98.9;
 
     // strcpy(ECE[0].name,"uttam");
     // printf("name is %s",ECE[0].name);
