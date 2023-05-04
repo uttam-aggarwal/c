423a572b97337632085ec55ff407bcffd4273f4d
uttam-aggarwal <115714793+uttam-aggarwal@users.noreply.github.com>
2023-05-04 23:12:40 +0530

    --

--- digitfrequency.c
+++ digitfrequency.c
@@ -8,7 +8,7 @@ int main() {
     char inp[200];
     char arr[10]={48,49,50,51,52,53,54,55,56,57};
     fgets(inp,200,stdin);
-    for(int i=0;i!="/0";i++){//
+    for(int i=0;i!="/0";i++){///
         
     }
     
